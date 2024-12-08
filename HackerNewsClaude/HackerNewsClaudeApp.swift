39aaa2365b20cd9d1c08296fdb9b27d1080c12c4
Wayne Dahlberg <waynefj40@gmail.com>
2024-12-07 18:58:50 -0700

    Initial Commit

---
+++ HackerNewsClaude/HackerNewsClaudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsClaudeApp.swift
+//  HackerNewsClaude
+//
+//  Created by Wayne Dahlberg on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsClaudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
